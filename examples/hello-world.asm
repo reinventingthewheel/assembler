90d92bd555c294e0de9e76cecffb602482cfbf16
Snit <snit.ram@gmail.com>
2012-03-24 04:07:51 -0300

    adding hello world to examples

---
+++ examples/hello-world.asm
@@ -0,0 +1,32 @@
+;Storing values on registers
+MOV A 72   ;H
+MOV B 101  ;e
+MOV C 108  ;l
+MOV D 108  ;l
+MOV E 111  ;o
+MOV F 32   ;\s
+
+;Storing values on memory addresses
+MOV 0 87   ;W
+MOV 1 111  ;o
+MOV 2 114  ;r
+MOV 3 108  ;l
+MOV 4 100  ;d
+MOV 5 10   ;\n
+
+;Writing values from registers to stdout
+WRITE A
+WRITE B
+WRITE C
+WRITE D
+WRITE E
+WRITE F
+
+;Writing values from memory to stdout
+WRITE 0
+WRITE 1
+WRITE 2
+WRITE 3
+WRITE 4
+WRITE 5
+
